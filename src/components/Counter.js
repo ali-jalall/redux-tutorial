b718bd3f110f27981254d1546c54d00de3ab65ac
alijalal-sys <ajmabilal@gmail.com>
2020-05-14 00:41:02 +0200

    initializing counter with redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,14 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
 class Counter extends React.Component {
-  state = { count: 0 }
-
   increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
-  }
+    this.props.dispatch({ type: "INCREMENT" });
+  };
 
   decrement = () => {
-    this.setState({
-      count: this.state.count - 1
-    });
-  }
+    this.props.dispatch({ type: "DECREMENT" });
+  };
 
   render() {
     return (
@@ -21,12 +16,18 @@ class Counter extends React.Component {
         <h2>Counter</h2>
         <div>
           <button onClick={this.decrement}>-</button>
-          <span>{this.state.count}</span>
+          <span>{this.props.count}</span>
           <button onClick={this.increment}>+</button>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Counter;
+function mapStateToProps(state) {
+  return {
+    count: state.count,
+  };
+}
+
+export default connect(mapStateToProps)(Counter);
